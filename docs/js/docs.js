417b776746fa3f9bc835b5611953a871956ade16
Gao Wang <gaow@uchicago.edu>
2017-03-30 18:17:44 -0500

    update

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,5 +1,5 @@
 var notebookDict={}
-var writeupDict={}
+var writeupDict={"Meetings-1":"Meetings","A-modular-approach-to-M&M-ASH-model-1":"Modular_MNMASH"}
 var images=[]
 var documentation=[]
 var writeup=["Meetings", "Modular_MNMASH"]
