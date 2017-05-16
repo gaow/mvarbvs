f48460587b90d95b5fe76ff2bb5fd3fc7aeaf936
Gao Wang <gaow@uchicago.edu>
2017-05-16 13:02:58 -0500

    Update website

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,5 +1,5 @@
 var notebookDict={"GTEx-V7-RNA-Seq-data-processing-1":"v7-rnaseq-preprocessing"}
 var writeupDict={"A-modular-approach-to-M&M-ASH-model-1":"Modular_MNMASH","Start-Simple!-1":"Start_Simple","GTEx-V7-eQTL-data-analysis-plan-1":"GTEx7_Analysis_Plan","Meetings-1":"Meetings"}
-var images=["reparam-pi.png","2tissues-model.png"]
+var images=["reparam-pi.png","UMichImputation.png","2tissues-model.png"]
 var documentation=["v7-rnaseq-preprocessing"]
 var writeup=["Meetings", "GTEx7_Analysis_Plan", "Modular_MNMASH", "Start_Simple"]
