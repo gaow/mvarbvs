e6dc6b21ee98dda9807b15165d3031b15f581004
Gao Wang <gaow@uchicago.edu>
2017-06-06 20:41:10 -0500

    Update website

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,5 +1,5 @@
-var notebookDict={"GTEx-V7-RNA-Seq-data-processing-1":"v7-rnaseq-preprocessing","GTEx-V7-genotype-missing-data-summary-1":"20170515-V7-Missing-Data","Match-sample-label-with-GTEx-provided-PCA-results-1":"20170523-GTEx-PCA-Label","Summary-of-imputation-outcome-1":"20170518-Imputation-Summary"}
+var notebookDict={"GTEx-V7-RNA-Seq-data-processing-1":"v7-rnaseq-preprocessing","GTEx-V7-genotype-missing-data-summary-1":"20170515-V7-Missing-Data","-Simulation-of-expression-data-given-genotypes-1":"Expression-Simulation","Match-sample-label-with-GTEx-provided-PCA-results-1":"20170523-GTEx-PCA-Label","-Simulation-of-quantitative-phenotype-given-genotypes-1":"MR-ASH-Simulation","Summary-of-imputation-outcome-1":"20170518-Imputation-Summary"}
 var writeupDict={"A-modular-approach-to-M&M-ASH-model-1":"Modular_MNMASH","Start-Simple!-1":"Start_Simple","GTEx-V7-eQTL-data-analysis-procedure-1":"GTEx7_Analysis_Plan","Meetings-1":"Meetings"}
-var images=["reparam-pi.png","UMichImputation.png","2tissues-model.png","UMichImputationResult.pdf"]
+var images=["reparam-pi.png","UMichImputation.png","ENSG00000264247.ld.png","2tissues-model.png","UMichImputationResult.pdf"]
 var documentation=["20170523-GTEx-PCA-Label", "20170518-Imputation-Summary", "20170515-V7-Missing-Data", "v7-rnaseq-preprocessing"]
 var writeup=["Meetings", "GTEx7_Analysis_Plan", "Modular_MNMASH", "Start_Simple"]
