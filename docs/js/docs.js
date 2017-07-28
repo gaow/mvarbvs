517f95355921bab5f03a3f678a871badc529ebcc
Gao Wang <gaow@uchicago.edu>
2017-07-28 10:02:34 -0500

    Build website via jnbinder

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
-var analysisArray = []
+var analysisArray = ['20170515_V7_Missing_Data', '20170515_V7_RNASeq_Preprocessing', '20170518_Imputation_Summary', '20170523_GTEx_PCA_Label', '20170615_MASHR_Benchmark', '20170630_Simulation_Results', 'MR_ASH_Example', 'MR_ASH_Simulation']
 var analysisDict = {"GTEx-V7-genotype-missing-data-summary-1":"20170515_V7_Missing_Data","GTEx-V7-RNA-Seq-data-processing-1":"20170515_V7_RNASeq_Preprocessing","Summary-of-imputation-outcome-1":"20170518_Imputation_Summary","Match-sample-label-with-GTEx-provided-PCA-results-1":"20170523_GTEx_PCA_Label","mashr-R-vs.-C-benchmark-1":"20170615_MASHR_Benchmark","mr-ash-simulation-data-analysis-1":"20170630_Simulation_Results","mr-ash-example-analysis-1":"MR_ASH_Example","Simulation-of-quantitative-phenotype-given-genotypes-1":"MR_ASH_Simulation"}
-var pipelineArray = []
+var pipelineArray = ['Pipelines', 'DataWrestling.pipeline', 'MatrixEQTL.pipeline', 'Misc.pipeline', 'ResourceManagement.pipeline', 'prep.pipeline', 'simulation.pipeline']
 var pipelineDict = {"m&m-ash-pipeline-execution-interface-1":"Pipelines"}
-var writeupArray = []
+var writeupArray = ['GTEx7_Analysis_Plan', 'Meetings', 'Modular_MNMASH', 'Start_Simple', 'Status_MM_ASH', 'Status_MR_ASH']
 var writeupDict = {"GTEx-V7-eQTL-data-analysis-procedure-1":"GTEx7_Analysis_Plan","Meetings-1":"Meetings","A-modular-approach-to-M&M-ASH-model-1":"Modular_MNMASH","Start-Simple!-1":"Start_Simple","Status-of-m&m-ash-project-1":"Status_MM_ASH","Status-of-mr-ash-project-1":"Status_MR_ASH"}
