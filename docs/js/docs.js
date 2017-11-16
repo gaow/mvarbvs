6e9dc37cfe18b65c04f0962516f70d492ca0898d
Gao Wang <gaow@uchicago.edu>
2017-11-16 10:48:11 -0600

    Build website via jnbinder

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
-var analysisArray = ['20170630_Simulation_Study', '20170624_Simulation_Procedures']
-var analysisDict = {"Simulation-of-quantitative-phenotype-given-genotypes-1":"20170624_Simulation_Procedures","mr-ash-simulation-ash-paper-scenarios-1":"20170630_Simulation_Study"}
-var prototypeArray = ['20170628_MR_ASH_Toy_Example', '20170615_MASHR_Benchmark']
-var prototypeDict = {"mashr-R-vs.-C++-benchmark-1":"20170615_MASHR_Benchmark","mr-ash-example-analysis-1":"20170628_MR_ASH_Toy_Example"}
+var analysisArray = ['20171103_MNMASH_Data', '20170630_Simulation_Study', '20170624_Simulation_Procedures']
+var analysisDict = {"Simulation-of-quantitative-phenotype-given-genotypes-1":"20170624_Simulation_Procedures","mr-ash-simulation-ash-paper-scenarios-1":"20170630_Simulation_Study","Prepare-toy-example-data-set-for-M&M-ASH-model-1":"20171103_MNMASH_Data"}
+var prototypeArray = ['20171103_MNMASH_Model', '20170628_MR_ASH_Toy_Example', '20170615_MASHR_Benchmark']
+var prototypeDict = {"mashr-R-vs.-C++-benchmark-1":"20170615_MASHR_Benchmark","mr-ash-example-analysis-1":"20170628_MR_ASH_Toy_Example","Prototype-of-VEM-in-M&M-ASH-model-1":"20171103_MNMASH_Model"}
 var writeupArray = ['Status_MR_ASH', 'Status_MM_ASH', 'Start_Simple', 'Modular_MNMASH', 'Meetings']
 var writeupDict = {"Meetings-1":"Meetings","A-modular-approach-to-M&M-ASH-model-1":"Modular_MNMASH","Start-Simple!-1":"Start_Simple","Status-of-m&m-ash-project-1":"Status_MM_ASH","Status-of-mr-ash-project-1":"Status_MR_ASH"}
