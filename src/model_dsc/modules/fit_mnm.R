f59eed9ad58def39305974e369bf6499f03f6dde
Gao Wang <gaow@uchicago.edu>
2018-04-10 12:48:59 -0500

    Get mashr updates work

--- src/model_dsc/modules/fit_mnm.R
+++ src/model_dsc/modules/fit_mnm.R
@@ -4,26 +4,26 @@ update_mash_model <- function(X, Y, V, fitted_g) {
   ## result contains 'PosteriorMean' 'PosteriorSD' 'lfdr' 'NegativeProb' 'lfsr'
   reg <- mm_regression(X, Y)
   mash_data <- mashr::mash_set_data(reg[1,,], Shat = reg[2,,], V = as.matrix(V))
-  return(mashr::mash_compute_posterior_matrices(fitted_g, mash_data))
+  return(mashr::mash(mash_data, g = fitted_g, fixg = TRUE, outputlevel=99))
 }
 
 ## Initialize storage for results
 p_alpha <- rep(1, ncol(data$X)) / ncol(data$X)
 alpha <- matrix(0, ncol(data$X), maxL)
-mu <- lapply(1:maxL, matrix, 0, ncol(data$X), ncol(data$Y))
+mu <- lapply(1:maxL, function(i) matrix(0, ncol(data$X), ncol(data$Y)))
 Xr <- matrix(0, nrow(data$Y), ncol(data$Y))
 for (l in 1:maxL) {
   ## remove the lth effect
-  Xr <- Xr - data$X %*% (alpha[l,] * mu[[l]])
+  Xr <- Xr - data$X %*% (alpha[,l] * mu[[l]])
   ## update mash model
   mout <- update_mash_model(data$X, data$Y - Xr, data$V, model$fitted_g)
   ## update fitted values
-  mu[[l]] <- mout$PosteriorMean
-  l10bf <- get_log10bf(mout)
+  mu[[l]] <- mout$result$PosteriorMean
+  l10bf <- mashr::get_log10bf(mout)
   alpha_post <- exp((l10bf - max(l10bf)) * log(10)) * p_alpha
-  alpha[l,] <- alpha_post / sum(alpha_post)
+  alpha[,l] <- alpha_post / sum(alpha_post)
   ## add back the updated lth effect
-  Xr <- Xr + data$X %*% (alpha[l,] * mu[[l]])
+  Xr <- Xr + data$X %*% (alpha[,l] * mu[[l]])
 }
 model$alpha <- alpha
 model$mu <- mu
