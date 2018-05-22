a5637a636677c489830fe2f38f7b3cbcb8e98692
Gao Wang <gaow@uchicago.edu>
2018-05-21 23:39:54 -0500

    Update susie benchmark

--- dsc/modules/fit_susie.R
+++ dsc/modules/fit_susie.R
@@ -9,14 +9,14 @@ for (r in 1:ncol(data$Y)) {
                                estimate_residual_variance = estimate_residual_variance, 
                                prior_variance=prior_var, 
                                intercept=FALSE)
-  fitted[[r]]$lfsr <- susieR:::lfsr_fromfit(fitted[[r]])
-  fitted[[r]]$n_in_CI <- susieR:::n_in_CI(fitted[[r]])
-  fitted[[r]]$in_CI <- susieR:::in_CI(fitted[[r]])
+  fitted[[r]]$lfsr <- susieR::susie_get_lfsr(fitted[[r]])
+  fitted[[r]]$n_in_CI <- susieR:::n_in_CS(fitted[[r]])
+  fitted[[r]]$in_CI <- susieR::susie_in_CS(fitted[[r]])
 }
 
 posterior <- list(PosteriorMean=do.call(cbind, lapply(1:length(fitted), function(i) susieR:::coef.susie(fitted[[i]]))),
                   lfsr=do.call(cbind, lapply(1:length(fitted), function(i) fitted[[i]]$lfsr)),
-                  alpha=do.call(cbind, lapply(1:length(fitted), function(i) fitted[[i]]$alpha)),
+                  alpha=lapply(1:length(fitted), function(i) fitted[[i]]$alpha),
                   in_CI=lapply(1:length(fitted), function(i) fitted[[i]]$in_CI),
                   n_in_CI=do.call(cbind, lapply(1:length(fitted), function(i) fitted[[i]]$n_in_CI))
                   )
