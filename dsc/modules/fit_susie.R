ed3a7fe1d20393d8b7f27cc0174859994d1dc571
Gao Wang <gaow@uchicago.edu>
2018-05-31 21:18:30 -0500

    Increase maxI and lower tol for susie

--- dsc/modules/fit_susie.R
+++ dsc/modules/fit_susie.R
@@ -8,15 +8,18 @@ for (r in 1:ncol(data$Y)) {
                                max_iter=maxI,
                                estimate_residual_variance = estimate_residual_variance, 
                                prior_variance=prior_var, 
-                               intercept=FALSE)
+                               intercept=FALSE,
+                               tol=1e-3)
   fitted[[r]]$lfsr <- susieR::susie_get_lfsr(fitted[[r]])
   fitted[[r]]$n_in_CI <- susieR:::n_in_CS(fitted[[r]])
   fitted[[r]]$in_CI <- susieR::susie_in_CS(fitted[[r]])
+  fitted[[r]]$niter <- susieR:::susie_get_niter(fitted[[r]])
 }
 
 posterior <- list(PosteriorMean=do.call(cbind, lapply(1:length(fitted), function(i) susieR:::coef.susie(fitted[[i]]))),
                   lfsr=do.call(cbind, lapply(1:length(fitted), function(i) fitted[[i]]$lfsr)),
                   alpha=lapply(1:length(fitted), function(i) fitted[[i]]$alpha),
                   in_CI=lapply(1:length(fitted), function(i) fitted[[i]]$in_CI),
-                  n_in_CI=do.call(cbind, lapply(1:length(fitted), function(i) fitted[[i]]$n_in_CI))
+                  n_in_CI=do.call(cbind, lapply(1:length(fitted), function(i) fitted[[i]]$n_in_CI)),
+                  niter = sapply(1:length(fitted), function(i) fitted[[i]]$niter)                              
                   )
--- dsc/modules/fit_susie.dsc
+++ dsc/modules/fit_susie.dsc
@@ -1,7 +1,7 @@
 fit_susie: fit_susie.R
   # Prior variance of nonzero effects.
   @CONF: R_libs = susieR@stephenslab/susieR
-  maxI: 50
+  maxI: 200
   maxL: 5
   estimate_residual_variance: FALSE, TRUE
   prior_var: 0.05, 0.1, 0.2, 0.4
