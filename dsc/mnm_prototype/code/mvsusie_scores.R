778eb8020322dfc2ddc62ce4357c6feb5f4ab3cf
zouyuxin <yuxin.zou.stat@gmail.com>
2021-01-07 22:25:21 -0600

    remove delta check in score function

--- dsc/mnm_prototype/code/mvsusie_scores.R
+++ dsc/mnm_prototype/code/mvsusie_scores.R
@@ -111,5 +111,5 @@ mvsusie_scores = function(m, true_coef, lfsr_cutoff = 0.05) {
               true_cond_discoveries = true_positive_condition_cs,
               false_neg_cond_discoveries = false_neg_condition_cs,
               avg_diff_eff_size_percentile = mean(estimate_diff),
-              converged = (m$convergence$delta >=0 && m$convergence$converged)))
+              converged = m$convergence$converged))
 }
