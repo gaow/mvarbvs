82d4ae945e4dc6bd5201fe3d264afe42276a2285
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-02-16 07:58:57 -0600

    Made some improvements to the susie vs. mvsusie 1-SNP CSs plot.

--- scripts/create_cs1snp_plot.R
+++ scripts/create_cs1snp_plot.R
@@ -1,13 +1,17 @@
 library(ggplot2)
 library(cowplot)
 library(ggrepel)
-dat <- read.csv("../output/blood_cell_traits/cs1snp.csv")
-p <- ggplot(dat,aes(x = susie,y = mvsusie,label = trait)) +
-  geom_point(shape = 21,color = "dodgerblue") +
-  geom_text_repel(size = 2.5,color = "darkblue",max.overlaps = Inf) +
+dat <- read.csv("../output/blood_cell_traits/cs1snp.csv",
+                comment.char = "")
+p <- ggplot(dat,aes(x = susie,y = mvsusie,label = trait,color = cell_type)) +
+  geom_point(shape = 20) +
+  geom_text_repel(size = 2.5,max.overlaps = Inf) +
   geom_abline(intercept = 0,slope = 1,color = "black",
               linetype = "dashed") +
   scale_x_continuous(limits = c(0,150),breaks = seq(0,400,50)) +
   scale_y_continuous(limits = c(0,335),breaks = seq(0,400,50)) +
+  scale_color_manual(values = c("dodgerblue","salmon","red","violet")) +
   coord_fixed() +
   theme_cowplot(font_size = 12)
+print(p)
+ggsave("../plots/susie_vs_mvsusie_num_cs1snp.eps",p,height = 4,width = 4)
