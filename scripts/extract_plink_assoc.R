0cb100f576624c210f049e397cef1984fd7bc781
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-02-19 23:20:46 -0600

    Made a few fixes to the extract_plink_assoc.R script.

--- scripts/extract_plink_assoc.R
+++ scripts/extract_plink_assoc.R
@@ -8,8 +8,8 @@ traits <- c("Basophill_perc","Eosinophill_perc","HLR_perc","Haemoglobin",
             "Lymphocyte_perc","MCV","MSCV","Monocyte_perc","Neutrophill_perc",
             "PDW","Platelet_count","Plateletcrit","RBC_count","RDW",
             "Reticulocyte_perc","WBC_count")
-chr <- 7
-pos <- c(47187499,47686974)
+chr <- 8
+pos <- c(118878922,119377965)
 plink <- NULL
 for (trait in traits) {
   cat(trait,"\n")
@@ -17,8 +17,9 @@ for (trait in traits) {
   dat <- fread(datfile,sep="\t",header = TRUE)
   class(dat) <- "data.frame"
   dat <- subset(dat,POS >= pos[1] & POS <= pos[2])
-  plink <- cbind(plink,dat$P)
+  plink <- cbind(plink,as.numeric(dat$P))
 }
 plink <- as.data.frame(plink)
-rownames(plink) <- plink$ID
+rownames(plink) <- dat$ID
 colnames(plink) <- traits
+print(t(t(sapply(plink,min))))
