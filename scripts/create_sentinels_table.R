77ee5b8ce4f23b755e1d743d45b8d5605fb3592f
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-10-20 10:29:26 -0500

    Added vuckovic_pip column to the blood_cell_traits_mvsusie_sentinels CSV; another fix to the susie_pip column.

--- scripts/create_sentinels_table.R
+++ scripts/create_sentinels_table.R
@@ -1,4 +1,5 @@
 # TO DO: Explain here what this script is for, and how to use it.
+library(readxl)
 
 # Load the susie_rss and mvsusie_rss blood cell trait fine-mapping results.
 susie_outfile   <- file.path("../output/blood_cell_traits",
@@ -14,8 +15,12 @@ mvsusie <- transform(mvsusie,
                      REF    = factor(REF),
                      ALT    = factor(ALT))
 
+# Read in the Vuckovic et al fine-mapping results (Supplementary Table
+# S5 of Vuckovic et al 2020).
+vuckovic <- read_excel("../output/blood_cell_traits/vuckovic_table_s5.xlsx")
+class(vuckovic) <- "data.frame"
+
 # Select SNPs with a PIP of 0.9 or more.
-susie   <- subset(susie,PIP > 0.9)
 mvsusie <- subset(mvsusie,PIP > 0.9)
 
 # Add a "susie_pip" column to the "mvsusie" data frame,
@@ -27,19 +32,31 @@ rows1 <- which(is.element(ids,mvsusie$ID))
 rows2 <- match(ids[rows1],mvsusie$ID)
 mvsusie[rows2,"susie_pip"] <- pip[rows1]
 
+# Add a "vuckovic_pip" column to the "mvsusie" data frame.
+vuckovic <- transform(vuckovic,rsID = factor(rsID))
+pip <- tapply(vuckovic$FM_PP,vuckovic$rsID,max)
+ids <- names(pip)
+mvsusie$vuckovic_pip <- as.numeric(NA)
+rows1 <- which(is.element(ids,mvsusie$ID))
+rows2 <- match(ids[rows1],mvsusie$ID)
+mvsusie[rows2,"vuckovic_pip"] <- pip[rows1]
+
 # Reorganize the columns and rows in the "mvsusie" data frame a bit.
 mvsusie <- mvsusie[,c("CHR","POS","ID","REF","ALT","maf","PIP","susie_pip",
-                      "Region","CS_trait")]
+                      "vuckovic_pip","Region","CS_trait")]
 names(mvsusie) <- c("chr","pos","id","ref","alt","maf","pip","susie_pip",
-                    "region","traits")
+                    "vuckovic_pip","region","traits")
 rows <- order(mvsusie$chr,mvsusie$pos)
 mvsusie <- mvsusie[rows,]
 rownames(mvsusie) <- NULL
 
+
+    
 # Write the "mvsusie" data frame to a CSV file.
 mvsusie <- transform(mvsusie,
-                     maf       = format(maf,digits = 4),
-                     pip       = round(pip,digits = 4),
-                     susie_pip = round(susie_pip,digits = 4))
+                     maf          = format(maf,digits = 4),
+                     pip          = round(pip,digits = 4),
+                     susie_pip    = round(susie_pip,digits = 4),
+                     vuckovic_pip = round(vuckovic_pip,digits = 4))
 write.csv(mvsusie,"blood_cell_traits_mvsusie_sentinels.csv",
           quote = FALSE,row.names = FALSE)
