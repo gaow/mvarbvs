f053f3d7546a673ed933dc42cc40077f24e2364e
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-11-22 15:03:23 -0600

    Moved some of the gchromvar analysis files.

--- scripts/plot_gregor_mvsusie.R
+++ scripts/plot_gregor_mvsusie.R
@@ -13,6 +13,7 @@ traits <- c("RBC","HGB","MCV","RDW","MSCV",          # mature red blood cell
 n <- length(traits)
 scatterplots <- vector("list",n)
 names(scatterplots) <- traits
+mvsusie_odds_all <- NULL
 for (trait in traits) {
 
   # Load the susie and mvsusie results.
@@ -26,15 +27,23 @@ for (trait in traits) {
                         stringsAsFactors = FALSE)
   mvsusie <- read.table(mvsusie_file,sep = " ",header = TRUE,
                         stringsAsFactors = FALSE)
+  susie   <- subset(susie,is.finite(odds))
+  mvsusie <- subset(mvsusie,is.finite(odds))
 
+  # Include the mvsusie results in the "mvsusie_odds_all" data frame.
+  beds <- mvsusie$Bed_File
+  mvsusie_odds_all <- rbind(mvsusie_odds_all,
+                            data.frame(bed   = mvsusie$Bed_File,
+                                       trait = trait,
+                                       odds  = mvsusie$odds,
+                                       stringsAsFactors = FALSE))
+  
   # Generate a scatterplot comparing the susie estimates vs. the
   # mvsusie estimates.
   pdat <- data.frame(bed     = susie$Bed_File,
                      susie   = susie$odds,
                      mvsusie = mvsusie$odds,
                      stringsAsFactors = FALSE)
-  rows <- which(with(pdat,is.finite(susie) & is.finite(mvsusie)))
-  pdat <- pdat[rows,]
   x <- c(pdat$susie,pdat$mvsusie)
   rows <- which(with(pdat,!(mvsusie > 5 & mvsusie/susie > 2)))
   pdat[rows,"bed"] <- ""
@@ -52,6 +61,25 @@ for (trait in traits) {
 }
 
 # Show the scatterplots and generate a PDF of the scatterplots.
-print(do.call("plot_grid",c(scatterplots,list(nrow = 5,ncol = 4))))
+# print(do.call("plot_grid",c(scatterplots,list(nrow = 5,ncol = 4))))
 # TO DO.
 
+# Here is a plot to compare the mvSuSiE enrichments across traits
+# *and* annotations.
+mvsusie_odds_all <- transform(mvsusie_odds_all,
+                              bed   = factor(bed,beds),
+                              trait = factor(trait,traits))
+p <- ggplot(mvsusie_odds_all,aes(x = trait,y = bed,color = odds)) +
+  geom_point(shape = 20) +
+  theme_cowplot(font_size = 9)
+print(p)
+
+res <- rep(0,16)
+names(res) <- traits[-17]
+for (i in traits[-17]) {
+  x <- subset(mvsusie_odds_all,trait == i)
+  y <- subset(mvsusie_odds_all,trait == "crosstrait")
+  res[i] <- cor(x$odds,y$odds)
+}
+
+hist(x$odds,n = 64,xlab = "trait/crosstrait")
